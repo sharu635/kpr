616958f995050023c360d2f289f1b677922dcc4b
sharu635 <sharumathi44aids@gmail.com>
2025-06-09 16:42:30 +0530

    fufufufuf

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log(5==='5');
+let i=0
+do{
+    i++;
+    console.log(i);
+  
+}while(i<5)
+function greet(name ,age){
+    return 'hello ${name} is ${age} old'
+}
+
+    console.log(greet('RONALDO',40));
+    const choices = ['rock','paper','scissor']
+    let computer = choices[Math.floor(Math.random() * 3)]
+    consolele.log(computer);
